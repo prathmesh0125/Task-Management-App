f35cd7771f9216a187afc1ae7e6ddf0b06841dd3
prathmesh0125 <bidveprathmesh590@gmail.com>
2024-07-11 11:35:31 +0530

    vercel deployment

--- backend/main.py
+++ backend/main.py
@@ -17,6 +17,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 def get_db():
     db = database.SessionLocal()
     try:
@@ -24,14 +25,23 @@ def get_db():
     finally:
         db.close()
 
+
+@app.get("/")
+def root_app():
+
+    return {"msg": "hello"}
+
+
 @app.get("/api/tasks", response_model=List[schemas.Task])
 def read_tasks(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     tasks = crud.get_tasks(db, skip=skip, limit=limit)
     return tasks
 
+
 @app.post("/api/tasks", response_model=schemas.Task)
 def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
     return crud.create_task(db=db, task=task)
+
 
 @app.put("/api/tasks/{task_id}", response_model=schemas.Task)
 def update_task(task_id: int, task: schemas.TaskUpdate, db: Session = Depends(get_db)):
@@ -39,6 +49,7 @@ def update_task(task_id: int, task: schemas.TaskUpdate, db: Session = Depends(ge
     if db_task is None:
         raise HTTPException(status_code=404, detail="Task not found")
     return db_task
+
 
 @app.delete("/api/tasks/{task_id}", response_model=schemas.Task)
 def delete_task(task_id: int, db: Session = Depends(get_db)):
